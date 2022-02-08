46d0d18fafd86355aaa1d2d27e3706851bd9a9c5
waseem-h <waseemhassanshahid@gmail.com>
2022-02-08 14:40:12 +0000

    Change .meta files order

--- pkg/manifestgen/template.go
+++ pkg/manifestgen/template.go
@@ -4,17 +4,20 @@ const manifestTemplate = `fx_version 'cerulean'
 game 'gta5'
 
 files {
-	{{ if .HasCarcols -}}
-	'data/carcols/*.meta',
+	{{ if .HasVehicles -}}
+	'data/vehicles/*.meta',
 	{{ end -}}
 	{{ if .HasCarvariations -}}
 	'data/carvariations/*.meta',
 	{{ end -}}
-	{{ if .HasContentUnlocks -}}
-	'data/contentunlocks/*.meta',
+	{{ if .HasCarcols -}}
+	'data/carcols/*.meta',
 	{{ end -}}
 	{{ if .HasHandling -}}
 	'data/handling/*.meta',
+	{{ end -}}
+	{{ if .HasContentUnlocks -}}
+	'data/contentunlocks/*.meta',
 	{{ end -}}
 	{{ if .HasVehicleLayouts -}}
 	'data/vehiclelayouts/*.meta',
@@ -22,34 +25,31 @@ files {
 	{{ if .HasVehicleModelsets -}}
 	'data/vehiclemodelsets/*.meta',
 	{{ end -}}
-	{{ if .HasVehicles -}}
-	'data/vehicles/*.meta',
-	{{ end -}}
 	{{ if .HasWeaponsFile -}}
 	'data/weaponsfile/*.meta',
 	{{ end }}
 }
 
+{{ if .HasHandling -}}
+data_file 'HANDLING_FILE' 'data/handling/*.meta'
+{{ end -}}
+{{ if .HasVehicles -}}
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles/*.meta'
+{{ end -}}
 {{ if .HasCarcols -}}
 data_file 'CARCOLS_FILE' 'data/carcols/*.meta'
 {{ end -}}
 {{ if .HasCarvariations -}}
 data_file 'VEHICLE_VARIATION_FILE' 'data/vehiclevariations/*.meta'
 {{ end -}}
-{{ if .HasContentUnlocks -}}
-data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks/*.meta'
-{{ end -}}
-{{ if .HasHandling -}}
-data_file 'HANDLING_FILE' 'data/handling/*.meta'
-{{ end -}}
 {{ if .HasVehicleLayouts -}}
 data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts/*.meta'
 {{ end -}}
+{{ if .HasContentUnlocks -}}
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks/*.meta'
+{{ end -}}
 {{ if .HasVehicleModelsets -}}
 data_file 'AMBIENT_VEHICLE_MODEL_SET_FILE' 'data/vehiclemodelsets/*.meta'
-{{ end -}}
-{{ if .HasVehicles -}}
-data_file 'VEHICLE_METADATA_FILE' 'data/vehicles/*.meta'
 {{ end -}}
 {{ if .HasWeaponsFile -}}
 data_file 'WEAPONINFO_FILE' 'data/weaponsfile/*.meta'
